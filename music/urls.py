62a851e94d08e77109e742ea0e3b0f5355297c55
Pranoy12001 <pranoy.mbstu12001@gmail.com>
2018-01-15 03:03:17 +0600

    function favourite is added in view.py and details.html is updated

--- music/urls.py
+++ music/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('', views.index,name='index'),
     #/music/album.id/
     path('<int:album_id>/',views.detail,name='detail'),
+    #/music/album.id/favorite/
+    path('<int:album_id>/favorite/',views.favorite,name='favorite'),
 ]
--- music/views.py
+++ music/views.py
@@ -7,3 +7,17 @@ def index(request):
 def detail(request,album_id):
     album=get_object_or_404(Album, pk=album_id)
     return render(request, 'music/details.html', {'allAlbum': album})
+
+def favorite(request,album_id):
+    album=get_object_or_404(Album,pk=album_id)
+    try:
+        selected_song=album.song_set.get(pk=request.POST['song'])
+    except (KeyError,Song.DoesNotExist):
+        return render(request,'music/details.html',{
+    'allAlbum':allAlbum,
+    'error_message' : "You didnot select a valid song",
+    })
+    else:
+        selected_song.is_favorite = True
+        selected_song.save()
+        return render(request,'music/details.html',{'allAlbum':album})
